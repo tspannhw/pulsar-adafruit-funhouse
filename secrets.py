0a57ce88ae7c7e20ef01afda526661689cc1e6f9
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-12-20 11:07:52 -0500

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'MySSIDNameIsWeird',
+    'password' : 'My1234Password56789Is#NotV$r3Good!At***Alll&^#$JJDddd..',
+    'aio_username' : 'none',
+    'aio_key' : 'none',
+    'mqtt_broker': "192.168.1.230",
+    'mqtt_port': 1883,
+    'mqtt_username': 'admin',
+    'mqtt_password': 'admin',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    }
